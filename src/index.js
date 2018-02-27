08515821469be18ccf95a8cc16e5898b3fad740e
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-02-28 00:00:32 +0300

    perf: use `neo-async` instead `async` (#31)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 */
 const fs = require('fs');
 const path = require('path');
-const async = require('async');
+const async = require('neo-async');
 const crypto = require('crypto');
 const mkdirp = require('mkdirp');
 
