1521447fdef2225b3b3e2922d881ac73755bc665
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2018-10-30 14:32:32 -0500

    Update src/index.js
    Co-Authored-By: jsg2021 <jsg2021@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -38,7 +38,10 @@ function loader(...args) {
 
   // Should the file get cached?
   let cache = true;
-
+// this.fs can be undefined 
+// e.g when using the thread-loader
+// fallback to the fs module
+const stat = this.fs ? this.fs.stat : fs.stat
   const toDepDetails = (dep, mapCallback) => {
     (this.fs || fs).stat(dep, (err, stats) => {
       if (err) {
