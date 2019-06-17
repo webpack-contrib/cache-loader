1ba0568f09967202085c3cfd055f933163a00482
Tiago Costa <tiagoffcc@hotmail.com>
2019-06-17 19:06:01 +0100

    fix(na): integration between eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ['@webpack-contrib/eslint-config-webpack', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
